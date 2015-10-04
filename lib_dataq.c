2f5dfa756543f390722dc4aedb8f1404bbe8190a
Rolf Winter <rolf.winter@gmail.com>
2015-10-04 18:40:53 +0200

    mostly beautification and error checking

--- lib_dataq.c
+++ lib_dataq.c
@@ -5,52 +5,64 @@
 #include <stdlib.h>
 
 /*
- * Opens the data logger device 
- * 
+ * Opens the data logger device
+ *
  * must be given device name as input parameter
  * e.g. /dev/ttyACM0
  *
- * the block parameter is used make I/O blocking or non-blocking 
- * 
+ * the block parameter is used make I/O blocking or non-blocking
+ *
  * returns file descriptor or on failure whatever
- * open() returns on failure (-1) 
+ * open() returns on failure (-1)
  */
 
-int open_dev(char* dev_name, bool block) {
+int dataq_open_dev(char *dev_name, bool block)
+{
+
+	if (dev_name == NULL) {
+		return -1;
+	}
 
 	int fd = -1;
 
 	fd = open(dev_name, O_RDWR | O_NOCTTY | O_NDELAY);
 
-	if(fd != -1 && block) {
-		fcntl(fd, F_SETFL, 0);
+	if (fd != -1) {
+		__dataq_init(fd);
+
+		if (block) {
+			fcntl(fd, F_SETFL, 0);
+		} else {
+			fcntl(fd, F_SETFL, FNDELAY);
+		}
 	}
 
-	if(fd != -1 && !block) {
-		fcntl(fd, F_SETFL, FNDELAY);
-	}
-
-	return fd;	
+	return fd;
 }
 
-int send_command(int fd, char* cmd, char* result, unsigned int size) {
+int __dataq_send_command(int fd, char *cmd, char *result, unsigned int size)
+{
+
+	if (fd < 0 || cmd == NULL || size == 0) {
+		return -1;
+	}
+
 	int bytes = 0;
 	int total_bytes = 0;
 
-	char* tmp_str = malloc(sizeof(char) * size);
+	char *tmp_str = malloc(sizeof(char) * size);
 
-	bytes = write(fd, cmd, strlen(cmd)); 
-	
+	bytes = write(fd, cmd, strlen(cmd));
+
 	//TODO: need to handle the case where bytes != strlen(cmd), i.e. the
 	//full command has not been written
-	if(bytes < 0) {
+	if (bytes < 0) {
 		return bytes;
 	}
-	
-	while ((bytes = read(fd, tmp_str + total_bytes, size - total_bytes - 1)) > 0)
-	{
-		total_bytes += bytes;	
-		
+
+	while ((bytes = read(fd, tmp_str + total_bytes, size - total_bytes - 1)) > 0) {
+		total_bytes += bytes;
+
 		if (tmp_str[total_bytes - 1] == '\r' || tmp_str[total_bytes - 1] == '\n') {
 			break;
 		}
@@ -58,7 +70,7 @@ int send_command(int fd, char* cmd, char* result, unsigned int size) {
 	}
 
 	tmp_str[total_bytes] = '\0';
-	
+
 	//now we remove the command, which is echoed in every response
 	strncpy(result, tmp_str + strlen(cmd), size);
 
@@ -68,20 +80,79 @@ int send_command(int fd, char* cmd, char* result, unsigned int size) {
 
 }
 
-int model(int fd, char* model_name, unsigned int size) {
-	return(send_command(fd, DATAQ_CMD_MODEL, model_name, size)); 
+int __dataq_init(int fd)
+{
+	if (fd < 0) {
+		return -1;
+	}
+
+	int size = strlen(DATAQ_CMD_ASC);
+	char data[size];
+
+	//potential TODO: look at the returned string
+	return (__dataq_send_command(fd, DATAQ_CMD_ASC, data, size));
 }
 
-int vendor(int fd, char* vendor_name, unsigned int size) {
-	return(send_command(fd, DATAQ_CMD_VENDOR, vendor_name, size)); 
+int dataq_model(int fd, char *model_name, unsigned int size)
+{
+	if (fd < 0 || model_name == NULL || size == 0) {
+		return - 1;
+	}
+
+	return (__dataq_send_command(fd, DATAQ_CMD_MODEL, model_name, size));
 }
 
-int firmware(int fd, char* firmware_version, unsigned int size) {
-	//potential TODO for the future: turn hex firmware version
-	//into decimal firmware version
-	return(send_command(fd, DATAQ_CMD_FIRMWARE, firmware_version, size));
+int dataq_vendor(int fd, char *vendor_name, unsigned int size)
+{
+	if (fd < 0 || vendor_name == NULL || size == 0) {
+		return -1;
+	}
+
+	return (__dataq_send_command(fd, DATAQ_CMD_VENDOR, vendor_name, size));
 }
 
-int serial_number(int fd, char* serial_number, unsigned int size) {
-	return(send_command(fd, DATAQ_CMD_SERIAL_NUM, serial_number, size));
+int dataq_firmware(int fd, char *firmware_version, unsigned int size)
+{
+	if (fd < 0 || firmware_version == NULL || size == 0) {
+		return -1;
+	}
+
+	//potential TODO: turn hex firmware version into decimal firmware version
+	return (__dataq_send_command(fd, DATAQ_CMD_FIRMWARE, firmware_version, size));
 }
+
+int dataq_serial_number(int fd, char *serial_number, unsigned int size)
+{
+	if (fd < 0 || serial_number == NULL || size == 0) {
+		return -1;
+	}
+
+	return (__dataq_send_command(fd, DATAQ_CMD_SERIAL_NUM, serial_number, size));
+}
+
+int dataq_start(int fd)
+{
+	if (fd < 0) {
+		return -1;
+	}
+
+	int size = strlen(DATAQ_CMD_START);
+	char data[size];
+
+	//potential TODO: look at the returned string
+	return (__dataq_send_command(fd, DATAQ_CMD_START, data, size));
+}
+
+int dataq_stop(int fd)
+{
+	if (fd < 0) {
+		return -1;
+	}
+
+	int size = strlen(DATAQ_CMD_STOP);
+	char data[size];
+
+	//potential TODO: look at the returned string
+	return (__dataq_send_command(fd, DATAQ_CMD_STOP, data, size));
+}
+
--- lib_dataq.h
+++ lib_dataq.h
@@ -10,13 +10,28 @@
 #define DATAQ_CMD_FIRMWARE "info 2\r"
 #define DATAQ_CMD_SERIAL_NUM "info 6\r"
 
-int open_dev(char* dev_name, bool block);
-int send_command(int fd, char* cmd, char* result, unsigned int size);
+#define DATAQ_CMD_SCAN_LIST_PREFIX "slist"
+#define DATAQ_CMD_SCAN_RATE_PREFIX "srate"
 
-int model(int fd, char* model_name, unsigned int size);
-int vendor(int fd, char* vendor_name, unsigned int size);
-int firmware(int fd, char* firmware_version, unsigned int size);
-int serial_number(int fd, char* serial_number, unsigned int size);
+#define DATAQ_CMD_ASC "asc\r"
+#define DATAQ_CMD_START "start\r"
+#define DATAQ_CMD_STOP "stop\r"
+
+#define DATAQ_EOC "\r" //end of command
+
+
+int __send_command(int fd, char *cmd, char *result, unsigned int size);
+int __dataq_init(int fd);
+
+int dataq_open_dev(char *dev_name, bool block);
+
+int dataq_model(int fd, char *model_name, unsigned int size);
+int dataq_vendor(int fd, char *vendor_name, unsigned int size);
+int dataq_firmware(int fd, char *firmware_version, unsigned int size);
+int dataq_serial_number(int fd, char *serial_number, unsigned int size);
+
+int dataq_start(int fd);
+int dataq_stop(int fd);
 
 
 #endif
